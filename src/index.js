550e67963823a52792592f1bfa977c5867f8aa06
Wutche Chikaome <wutchechikaome75@gmail.com>
2023-01-06 11:48:41 +0100

    applying spread operator

--- src/index.js
+++ src/index.js
@@ -6,31 +6,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 //setup vars
 const books = [
-	{
+	{id: 1,
+		img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg",
 		title: "I Love you to the moon and back",
-		image: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg",
 		author: "Amelia Hepworth",
 	},
 
-	{
+	{id:2,
+		img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
 		title: "Our Class is a Family",
-		image: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
 		author: "Shannon Olsen ",
+	},
+	{id:3,
+		img: "https://m.media-amazon.com/images/I/81wgcld4wxL._AC_UY218_.jpg",
+		title: "Atomic Habits",
+		author: "James Clear ",
 	},
 ];
 
 function BookList() {
 	return (
 		<section className="booklist">
-			{/* <Book
-				img={firstBoook.image}
-				title={firstBoook.title}
-				author={firstBoook.author}></Book>
-			<Book
-				img={secondBoook.image}
-				title={secondBoook.title}
-				author={secondBoook.author}
-			/> */}
+			{books.map((book) => {
+				return <Book key = {book.id} {...book}></Book>;
+			})}
 		</section>
 	);
 }
@@ -48,3 +47,19 @@ const Book = (props) => {
 };
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
+
+/* <Book
+				img={firstBoook.image}
+				title={firstBoook.title}
+				author={firstBoook.author}></Book>
+			<Book
+				img={secondBoook.image}
+				title={secondBoook.title}
+				author={secondBoook.author}
+			/> */
+
+// const names = ["John", "James", "Susan"];
+// const newNames = names.map((name) => {
+// 	return<h1>{name}</h1>
+// });
+// console.log(newNames);
